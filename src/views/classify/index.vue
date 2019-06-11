f72b5c367c6a422ece1d06a485ae39294da285fc
gankai <gankai@jc.com>
2019-06-11 15:32:40 +0800

    完成分类右侧滑动导航栏的初步制作

--- src/views/classify/index.vue
+++ src/views/classify/index.vue
@@ -2,47 +2,117 @@
   <div class="classify">
     <p>商品分类</p>
     <div class="munu_all">
-      <ul class="meun_classify wrapper" ref="wrapper">
-        <van-sidebar :active-key="activeKey" @change="onChange">
-          <van-sidebar-item title="运动 户外"/>
-          <van-sidebar-item title="美妆 护肤"/>
-          <van-sidebar-item title="个护 清理"/>
-          <van-sidebar-item title="钟表 珠宝"/>
-          <van-sidebar-item title="运动 户外"/>
-          <van-sidebar-item title="美妆 护肤"/>
-          <van-sidebar-item title="个护 清理"/>
-          <van-sidebar-item title="钟表 珠宝"/>
-          <van-sidebar-item title="运动 户外"/>
-          <van-sidebar-item title="美妆 护肤"/>
-          <van-sidebar-item title="个护 清理"/>
-          <van-sidebar-item title="钟表 珠宝"/>
-          <van-sidebar-item title="运动 户外"/>
-          <van-sidebar-item title="美妆 护肤"/>
-          <van-sidebar-item title="个护 清理"/>
-          <van-sidebar-item title="钟表 珠宝"/>
-          <van-sidebar-item title="运动 户外"/>
-          <van-sidebar-item title="美妆 护肤"/>
-          <van-sidebar-item title="美妆 护肤"/>
-        </van-sidebar>
-      </ul>
-      <ul class="classify_content">
-        <li v-for="(list,index) in lists" :key="index">
-          <img :src="list.image">
-          <p>{{list.label}}</p>
-        </li>
-      </ul>
+      <div class="meun_classify" ref="meun_classify">
+        <ul>
+          <van-sidebar :active-key="activeKey" @change="onChange">
+            <van-sidebar-item title="运动 户外"/>
+            <van-sidebar-item title="美妆 护肤"/>
+            <van-sidebar-item title="个护 清理"/>
+            <van-sidebar-item title="钟表 珠宝"/>
+            <van-sidebar-item title="运动 户外"/>
+            <van-sidebar-item title="美妆 护肤"/>
+            <van-sidebar-item title="个护 清理"/>
+            <van-sidebar-item title="钟表 珠宝"/>
+            <van-sidebar-item title="运动 户外"/>
+            <van-sidebar-item title="美妆 护肤"/>
+            <van-sidebar-item title="个护 清理"/>
+            <van-sidebar-item title="钟表 珠宝"/>
+            <van-sidebar-item title="运动 户外"/>
+            <van-sidebar-item title="美妆 护肤"/>
+            <van-sidebar-item title="个护 清理"/>
+            <van-sidebar-item title="钟表 珠宝"/>
+            <van-sidebar-item title="运动 户外"/>
+            <van-sidebar-item title="美妆 护肤"/>
+            <van-sidebar-item title="美妆 护肤"/>
+          </van-sidebar>
+        </ul>
+      </div>
+      <div class="classify_content" ref="classify_content">
+        <ul>
+          <li v-for="(list,index) in lists" :key="index">
+            <img :src="list.image">
+            <p>{{list.label}}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import BScroll from 'better-scroll'
+import BScroll from "better-scroll";
 export default {
   name: "",
   data() {
     return {
       activeKey: 0,
       lists: [
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
+        {
+          url: "https://m.cdclass.net",
+          image:
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/bannner/1901/learn.png",
+          label: "分类一"
+        },
         {
           url: "https://m.cdclass.net",
           image:
@@ -90,11 +160,12 @@ export default {
     const meun_classify = document.querySelector(".meun_classify");
     const classify_content = document.querySelector(".classify_content");
     const bodyheight = document.documentElement.clientHeight;
-    meun_classify.style.height = bodyheight - 10 + "px";
-    classify_content.style.height = bodyheight - 10 + "px";
+    meun_classify.style.height = bodyheight - 50 + "px";
+    classify_content.style.height = bodyheight - 50 + "px";
     this.$nextTick(() => {
-        this.scroll = new BScroll(this.$refs.wrapper, {})
-      })
+      this.scroll_menu = new BScroll(this.$refs.meun_classify, {});
+      this.scroll_content = new BScroll(this.$refs.classify_content, {});
+    });
   }
 };
 </script>
@@ -106,28 +177,37 @@ export default {
     .meun_classify {
       width: 25%;
       flex-wrap: wrap;
-      // overflow: scroll;
-    }
-    .meun_classify::-webkit-scrollbar {
-      width: 0 !important;
-    }
-    .classify_content::-webkit-scrollbar {
-      width: 0 !important;
+      li {
+        height: 50px;
+        line-height: 50px;
+        border-bottom: 1px solid #ffffff;
+        color: #333;
+        background-color: #f8f8f8;
+        font-size: 14px;
+      }
     }
     .classify_content {
       width: 75%;
-      display: flex;
-      flex-wrap: wrap;
-      // overflow: scroll;
-      li {
-        width: 50%;
-        height: 150px;
-        justify-content: center;
-        align-items: center;
-        font-size: 15px;
-        img {
-          width: 80px;
-          height: 80px;
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+
+        li {
+          width: 50%;
+          justify-content: center;
+          align-items: center;
+          font-size: 15px;
+
+          img {
+            width: 80px;
+            height: 80px;
+          }
+
+          .cubeic-add,
+          .cubeic-remove {
+            font-size: 18px;
+            color: red;
+          }
         }
       }
     }
