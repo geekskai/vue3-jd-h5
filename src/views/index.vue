b67475e8184375a054163be0ff8ec616692803ec
gankai <gankai@jc.com>
2019-07-22 10:27:46 +0800

    fix(去除点击路由左右滑动的功能): 路由动画去除
    将router-view标签上的transform标签去除
    
    BREAKING CHANGE: 去除了所有页面的路由左右进入的动画
    
    re#1

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="index">
-    <transition :name="transitionName">
-      <router-view class="router-view" />
-    </transition>
+    <!-- <transition :name="transitionName"> -->
+    <router-view class="router-view" />
+    <!-- </transition> -->
   </div>
 </template>
 
