58822a6071daae92429f6691695ee3979e382563
gankai <gankai@jc.com>
2019-07-23 11:02:37 +0800

    fix(处理新品首发的直接banner图): 处理banner
    完成新品首发的 直接的商品的轮播图的样式
    
    BREAKING CHANGE: 改变新品首发页面的轮播图

--- src/views/newProductLaunch/index.vue
+++ src/views/newProductLaunch/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="new-product-launch">
     <header class="page-header">
-      <span class="green-btn" @click="$router.go(-1)">
-        <svg-icon icon-class="transparent-btn"></svg-icon>
+      <span class="btn-left" @click="$router.go(-1)">
+        <svg-icon icon-class="green-btn"></svg-icon>
       </span>
       <div class="header-content">新品首发</div>
       <span class="share-btn">
@@ -17,6 +17,49 @@
         </swiper-slide>
       </swiper>
     </div>
+
+    <div class="page-tabs">
+      <van-tabs
+        :swipe-threshold="4"
+        title-inactive-color="#3a3a3a"
+        title-active-color="#D8182D"
+        background="#FFFFFF"
+        v-model="active"
+        animated
+        swipeable
+        :line-width="0"
+      >
+        <van-tab v-for="(item,index) in tabList" :title="item.name" :key="index">
+          <!-- <div class="slot-box"> -->
+          <div class="slot-title" slot="title">
+            <p class="tab-title">{{item.title}}</p>
+            <span class="tab-name">{{item.name}}</span>
+          </div>
+          <!-- </div> -->
+          <section class="goods-box search-wrap">
+            <ul class="goods-content">
+              <li v-for="(item,index) in list" :key="index">
+                <router-link tag="div" to="/classify/product">
+                  <img :src="item.img" />
+                </router-link>
+                <div class="goods-layout">
+                  <div class="goods-title">{{item.name}}</div>
+                  <span class="goods-div">{{item.title}}</span>
+                  <div class="goods-desc">
+                    <span class="goods-price">
+                      <i>{{item.price}}</i>
+                    </span>
+                    <span class="add-icon" @click="addToCart($event,item)">
+                      <svg-icon icon-class="add"></svg-icon>
+                    </span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </section>
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
 
@@ -25,6 +68,67 @@ export default {
   name: "newProductLaunch", // 新品首发
   data() {
     return {
+      active: "1",
+      tabList: [
+        {
+          title: "精选",
+          name: "最近很火"
+        },
+        {
+          title: "手机",
+          name: "新机速递"
+        },
+        {
+          title: "电器",
+          name: "3C家电"
+        },
+        {
+          title: "时尚",
+          name: "美妆穿搭"
+        },
+        {
+          title: "生活",
+          name: "居家日用"
+        }
+      ],
+      list: [
+        {
+          name: "多功能料理机",
+          img: require("../../assets/image/home/test1.png"),
+          title: "限量套装 新品上市",
+          price: "$125"
+        },
+        {
+          name: "遥控制空调扇",
+          img: require("../../assets/image/home/test2.png"),
+          title: "限量套装 新品上市",
+          price: "$245"
+        },
+        {
+          name: "时尚双肩包",
+          img: require("../../assets/image/home/test3.png"),
+          title: "限量套装 新品上市",
+          price: "$21"
+        },
+        {
+          name: "商务行李箱",
+          img: require("../../assets/image/home/test4.png"),
+          title: "限量套装 新品上市",
+          price: "$218"
+        },
+        {
+          name: "无线消噪耳机",
+          img: require("../../assets/image/home/test5.png"),
+          title: "限量套装 新品上市",
+          price: "$218"
+        },
+        {
+          name: "无线蓝牙耳机",
+          img: require("../../assets/image/home/test6.png"),
+          title: "限量套装 新品上市",
+          price: "$218"
+        }
+      ],
       images2: [
         {
           imgUrl: require("../../assets/image/home/banner5.jpg"),
@@ -46,7 +150,6 @@ export default {
       swiperOption: {
         loop: true,
         autoplay: false,
-
         effect: "coverflow",
         coverflowEffect: {
           rotate: 0, //slide做3d旋转时Y轴的旋转角度。默认50。
@@ -76,10 +179,10 @@ export default {
     justify-content: flex-start;
     align-items: flex-start;
     padding: 20px;
-    .return-btn {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    .btn-left {
+      position: fixed;
+      left: 16px;
+      top: 14px;
     }
     .header-content {
       text-align: center;
@@ -107,15 +210,112 @@ export default {
       align-items: center;
     }
     .swiper-slide-prev {
-      right: -28%;
+      right: -16%;
     }
     .swiper-slide-next {
-      left: -8%;
+      left: -5%;
     }
     .slide-img {
-      width: 300px;
+      width: 333px;
       height: 200px;
       border-radius: 5px;
+    }
+  }
+  .page-tabs {
+    /deep/ .van-tabs--line .van-tabs__wrap {
+      height: 50px;
+    }
+    /deep/ .van-tab {
+      line-height: 18px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    /deep/ .van-tab--active {
+      display: flex;
+      justify-content: center;
+      .tab-title {
+        border: 1px solid #d8182d;
+        border-radius: 10px 10px;
+      }
+    }
+    .slot-title {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .tab-title {
+        font-size: 14px;
+        font-weight: 600;
+        display: inline-block;
+        width: 40px;
+      }
+      .tab-name {
+        font-size: 10px;
+        display: inline-block;
+      }
+    }
+
+    .goods-box {
+      padding: 16px;
+      .good-things {
+        font-size: 18px;
+        color: #d8182d;
+        font-weight: 600;
+      }
+      .goods-content {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        li {
+          display: inline-block;
+          width: 165px;
+          margin-top: 10px;
+          border-radius: 8px;
+          background-color: white;
+          box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.1);
+          // background: url('../../assets/image/back-show.png') no-repeat center center;
+          // background-size:100% 100%;
+          img {
+            width: 100%;
+          }
+        }
+        li:nth-of-type(even) {
+          padding-right: 0;
+        }
+        .goods-layout {
+          width: 165px;
+          padding: 0 10px;
+          display: flex;
+          justify-content: flex-start;
+          flex-direction: column;
+          .goods-title {
+            color: #3a3a3a;
+            font-size: 14px;
+            font-weight: 600;
+          }
+          .goods-div {
+            color: #949497;
+            font-size: 11px;
+          }
+          .goods-desc {
+            background-color: #fff;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding-bottom: 10px;
+            .goods-price {
+              font-size: 14px;
+              color: #d8182d;
+            }
+            .add-icon {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+          }
+        }
+      }
     }
   }
 }
