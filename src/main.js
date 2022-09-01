43968cb031ae1a0ed892961e7e847f04142c22d2
vikoala <vikoala@foxmail.com>
2022-09-01 16:40:16 +0800

    refactor: add registerPlugins function

--- src/main.js
+++ src/main.js
@@ -8,28 +8,20 @@
  * @FilePath: /refactor-with-vue3/src/main.js
  */
 import { createApp } from 'vue'
+import Vant from 'vant'
+import 'lib-flexible/flexible'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import axios from "@/plugins/axios";
-
-// import SvgIcon from '@/components/SvgIcon'// svg组件
-import 'lib-flexible/flexible'
-
 import icons from '@/icons' // icon
 import components from '@/components' // icon
-
-import Vant from 'vant'
 import 'vant/lib/index.css' // 全局引入样式
-
 import './mock/mock'
-
 import eventBus from './utils/eventBus'
+import { registerPlugins } from './utils/util'
 
+const plugins = [Vant, store, router, axios, icons, components, eventBus]
 const app = createApp(App);
-app.use(Vant).use(store).use(router).mount('#app');
-app.use(axios)
-app.use(icons)
-app.use(components)
-app.use(eventBus)
+app.use(registerPlugins(plugins))
+app.mount("#app")
